b1ee0ebf488342f57aec619a22bb3f194d5ced76
Aalaizha <l.bnjamin0309@gmail.com>
2022-09-29 14:56:39 -0400

    Clean up firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -1,26 +1,4 @@
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  onAuthStateChanged,
-  signOut,
-} from "firebase/auth";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  query,
-  orderBy,
-  limit,
-  onSnapshot,
-  setDoc,
-  updateDoc,
-  doc,
-  serverTimestamp,
-} from "firebase/firestore";
-import { showSignedInNavBar, showSignedOutNavBar } from "./src/dom";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCSl1_8J03A-a594iZoWaOjYWjDeESrsAE",
@@ -33,77 +11,15 @@ const firebaseConfig = {
 
 // Initialize  Firebase
 initializeApp(firebaseConfig);
-initFirebaseAuth();
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore();
 
-export async function signIn() {
-  const provider = new GoogleAuthProvider();
-  await signInWithPopup(getAuth(), provider);
-}
-
-export function signOutUser() {
-  // Sign out of Firebase.
-  signOut(getAuth());
-}
-// Initialize firebase auth
-function initFirebaseAuth() {
-  // Listens to state changes - If user is signed in or signed out.
-  onAuthStateChanged(getAuth(), authStateObserver);
-}
-
-//Handles what happens in different auth states
-function authStateObserver(user) {
-  if (user) {
-    // User is signed in!
-    console.log("You're signed In");
-    showSignedInNavBar(user.displayName);
+export function getFirebaseConfig() {
+  if (!firebaseConfig || !firebaseConfig.apiKey) {
+    throw new Error(
+      "No Firebase configuration object provided." +
+        "\n" +
+        "Add your web app's configuration object to firebase-config.js"
+    );
   } else {
-    // User is signed out!
-    console.log("You're signed out");
-    showSignedOutNavBar();
+    return firebaseConfig;
   }
-}
-
-function getUserName() {
-  return getAuth().currentUser.displayName;
-}
-
-function getUserUID() {
-  return getAuth().currentUser.uid;
-}
-
-const form = document.querySelector(".form");
-
-// Saves a new message to Cloud Firestore.
-export async function saveReview(titleText, reviewText) {
-  // Add a new message entry to the Firebase database.
-  try {
-    await addDoc(collection(getFirestore(), "reviews"), {
-      name: getUserName(),
-      bookTitle: titleText,
-      text: reviewText,
-      timestamp: serverTimestamp(),
-      uid: getUserUID(),
-    });
-  } catch (error) {
-    console.error("Error writing new message to Firebase Database", error);
-  }
-}
-
-const colRef = collection(db, "reviews");
-
-//Gets the data from the firestore collection
-async function getData() {
-  await getDocs(colRef)
-    .then((data) => {
-      let reviews = [];
-      data.docs.forEach((doc) => {
-        reviews.push({ ...doc.data(), id: doc.id });
-      });
-      console.log(reviews);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 }
